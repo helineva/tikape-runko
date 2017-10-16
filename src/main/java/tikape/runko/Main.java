721391ac113c474c794522bcdb1f300bbfc19abe
Teemu Helineva <teemu.helineva@gmail.com>
2017-10-16 14:30:45 +0300

    Muokattu luokkia Main.java ja Database.java

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Database database = new Database("jdbc:sqlite:/db/smoothie.db");
-        database.init();
 
         DaoAine daoAine = new DaoAine(database);
 
