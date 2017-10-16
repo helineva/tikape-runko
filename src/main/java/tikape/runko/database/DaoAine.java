977c5773f50db582a213266bdc9a63e0e599884f
Hermanni Keko <hermanni.keko@aalto.fi>
2017-10-16 14:41:55 +0300

    loput DaoAine.java metodit

--- src/main/java/tikape/runko/database/DaoAine.java
+++ src/main/java/tikape/runko/database/DaoAine.java
@@ -9,8 +9,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import tikape.runko.domain.Aine;
+
 
 
 public class DaoAine implements Dao<Aine, Integer>{
@@ -23,7 +25,7 @@ public class DaoAine implements Dao<Aine, Integer>{
     @Override
     public Aine findOne(Integer key) throws SQLException {
         Connection connection = database.getConnection();
-        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Aine WHERE id = ?");
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM RaakaAine WHERE id = ?");
         statement.setInt(1, key);
         ResultSet rs = statement.executeQuery();
         if(!rs.next()) {
@@ -44,7 +46,37 @@ public class DaoAine implements Dao<Aine, Integer>{
 
     @Override
     public List<Aine> findAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection connection = database.getConnection();
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM RaakaAine");
+        ResultSet rs = statement.executeQuery();
+        List<Aine> aineet = new ArrayList<>();
+        if(!rs.next()) {
+            return null;
+        }
+        
+        while(rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+            Aine aine = new Aine(id, nimi);
+            aineet.add(aine);
+            
+            
+        }
+        rs.close();
+        statement.close();
+        connection.close();
+        return aineet;
+    }
+    
+    public void save(String nimi) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO RaakaAine(nimi) VALUES(?)");
+        statement.setString(1, nimi);
+        statement.executeUpdate();
+        statement.close();
+        connection.close();
+
+        
     }
 
     @Override
